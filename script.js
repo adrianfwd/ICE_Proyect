55bba27798aeda245dc7dc057b71f499a250e1f6
adrianfwd <azuniga@fwdcostarica.com>
2024-07-18 11:35:48 -0600

    si

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 7000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000);
 }
